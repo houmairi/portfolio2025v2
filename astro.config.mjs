83d244d4da078b5e86ca5b0e680f152edaf88ced
arjbhogal <bhogalarjun@gmail.com>
2025-07-08 09:09:26 +0200

    Added Default SEO to Layout.astro, added robots.txt to protect CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bhogal.at',
   vite: {
     plugins: [tailwindcss()]
 
-  }
+  },
+  integrations: [sitemap()]
 });
